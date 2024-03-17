d3895c89eee6fffca327fd9d67826a3b906ecab1
Gerhard <gdorner.gd@gmail.com>
2024-03-17 18:13:55 +0100

    2 demo funktioniert

--- haccp/views.py
+++ haccp/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-
+import datetime
 from django.views.generic import ListView
 from .models import ToDoItem
 
@@ -7,3 +6,13 @@ from .models import ToDoItem
 class AllToDos(ListView):
     model = ToDoItem
     template_name = "index.html"
+
+    def get_queryset(self):
+        return ToDoItem.objects.filter(due_date__gte=datetime.date.today())
+
+class TodayToDos(ListView):
+    model = ToDoItem
+    template_name = "today.html"
+
+    def get_queryset(self):
+        return ToDoItem.objects.filter(due_date=datetime.date.today())
